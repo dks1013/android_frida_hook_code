9b813c61a18a266021831a63cbdaa2223e53053f
geru <59687167+dks1013@users.noreply.github.com>
2026-02-01 11:23:13 +0900

    Create java_method_hook.js

---
+++ java_method_hook.js
@@ -0,0 +1,7 @@
+Java.perform(function () {
+	var test = Java.use("viva.republica.toss.splash.SplashActivity");
+	test.write.implementation = function(){
+		console.log('1111111111111111111111');
+		
+	}
+})
